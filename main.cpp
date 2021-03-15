2e8fdec4ffaeff79fb0c9e79325c6d34810fd18c
natali1821 <ermolina_natali@bk.ru>
2021-03-15 11:24:51 +0300

    Add main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include "Rational.h"
+#include <iostream>
+
+int main() {
+    Rational x; // 1/1
+    Rational y(10, 5); // 10/5
+    Rational z(7);
+
+    std::cout << "x.eq(y): ";
+    std::cout << x.eq(y) << std::endl;
+    std::cout << "x.neq(y): ";
+    std::cout << x.neq(y) << std::endl;
+    std::cout << "z.neg(): ";
+    z.neg().print();
+
+    Rational n(10, 0);
+    n.mul(Rational(1)).print();
+    Rational(1).mul(n).print();
+}
+
